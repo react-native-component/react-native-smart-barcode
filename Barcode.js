58cbb4fd9c5865eb6166acc828b06032523a4260
windwang <windwang@gmail.com>
2017-10-20 15:55:29 +0800

    update to RN0.49

--- Barcode.js
+++ Barcode.js
@@ -7,7 +7,7 @@
 
 
 import React, {
-    PropTypes,
+   
     Component,
 } from 'react'
 import {
@@ -17,6 +17,7 @@ import {
     AppState,
     Platform,
 } from 'react-native'
+import PropTypes from 'prop-types'
 
 const BarcodeManager = Platform.OS == 'ios' ? NativeModules.Barcode : NativeModules.CaptureModule
 
